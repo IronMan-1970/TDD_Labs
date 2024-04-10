161c7e142a973987e9b22d22fe032fc1818e01d5
Antihetman <umaks6859@gmail.com>
2024-04-10 11:18:18 +0300

    Lab 2

--- src/main/java/org/example/Convertor.java
+++ src/main/java/org/example/Convertor.java
@@ -6,30 +6,48 @@ package org.example;
   @version  1.0.0
   @since 4/9/24 - 13.21
 */
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
+
 
 public class Convertor {
 
-    public  static String convertToRomanNumerals(int number){
-        if ((number > 4000)|| (number <= 0)){
-            return null;
-        }
-        List<RomanNumerals> romanNumerals = RomanNumerals.returnSortedList();
-        int i = 0;
-        String romanNumeral = "";
-        while(number > 0){
-            if (number >= romanNumerals.get(i).returnValue())
-            {
-                romanNumeral = romanNumeral + romanNumerals.get(i).toString();
-                number -= romanNumerals.get(i).returnValue();
-            }
-            else
-            {
-                i++;
-            }
-        }
-        return romanNumeral;
-    }
+     public static int convertRomanToArabic(String romanNumeral) {
+           if (romanNumeral == null) {
+                 throw new IllegalArgumentException(" cannot be converted to a Roman Numeral");
+           }
+
+           if (romanNumeral.length() == 0) {
+                throw new IllegalArgumentException(" cannot be converted to a Roman Numeral");
+          }
+
+         romanNumeral.toUpperCase();
+         int result = 0;
+
+         List<RomanNumerals> romanNumerals = RomanNumerals.returnSortedList();
+         int i = 0;
+
+         while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+             RomanNumerals symbol = romanNumerals.get(i);
+             if (romanNumeral.startsWith(symbol.name())) {
+                 result += symbol.returnValue();
+                 romanNumeral = romanNumeral.substring(symbol.name().length());
+             }
+             else {
+                  i++;
+             }
+         }
+
+          if (romanNumeral.length() > 0) {
+                throw new IllegalArgumentException(" cannot be converted to a Roman Numeral");
+          }
+
+          return result;
+
+     }
 
 }

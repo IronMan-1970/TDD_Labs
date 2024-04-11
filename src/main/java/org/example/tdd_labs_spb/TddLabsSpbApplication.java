fa04e500dd342b0286bae71c4fdb9efb09d887db
Antihetman <umaks6859@gmail.com>
2024-04-11 10:49:10 +0300

    lab 4

---
+++ src/main/java/org/example/tdd_labs_spb/TddLabsSpbApplication.java
@@ -0,0 +1,13 @@
+package org.example.tdd_labs_spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddLabsSpbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddLabsSpbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tdd_labs_spb/TddLabsSpbArchitectureTests.java
@@ -0,0 +1,10 @@
+package org.example.tdd_labs_spb;/*
+  @author   antihetman
+  @project   TDD_Labs_Spb
+  @class  TddLabsSpbArchitectureTests
+  @version  1.0.0 
+  @since 4/11/24 - 10.40
+*/
+
+public class TddLabsSpbArchitectureTests {
+}
